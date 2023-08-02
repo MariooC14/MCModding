cb22482256da2a88c047df6abf715721f7a9ab96
MariooC14 <cavalmario2003@gmail.com>
2023-08-02 23:14:19 +0100

    Added Clover as a fuel item

---
+++ src/main/java/net/marioc14/learningmod/item/custom/FuelItem.java
@@ -0,0 +1,20 @@
+package net.marioc14.learningmod.item.custom;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.jetbrains.annotations.Nullable;
+
+// This is the class for creating your custom fuel item
+public class FuelItem extends Item {
+    private int burnTime = 0;
+    public FuelItem(Properties properties, int burnTime) {
+        super(properties);
+        this.burnTime = burnTime;
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+        return this.burnTime;
+    }
+}
